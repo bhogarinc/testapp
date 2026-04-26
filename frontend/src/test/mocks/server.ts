69d883cbfd2d6408953ef2231e983517fe05f1b8
Bhogar Inc <bhogarinc@gmail.com>
2026-04-26 07:40:52 +0000

    Add MSW server configuration

---
+++ frontend/src/test/mocks/server.ts
@@ -0,0 +1,10 @@
+/**
+ * MSW Server Setup
+ * 
+ * Configures Mock Service Worker for testing.
+ */
+
+import { setupServer } from 'msw/node';
+import { handlers } from './handlers';
+
+export const server = setupServer(...handlers);
